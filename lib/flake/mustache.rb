2e7362a9727491574977f8bcd9cf3e798b9ea9e7
Jeremy Ruppel <jeremy.ruppel@gmail.com>
2012-02-03 13:24:14 -0800

    Allow overrides to be falsy

--- lib/flake/mustache.rb
+++ lib/flake/mustache.rb
@@ -1,16 +1,16 @@
 module Flake
   class Mustache
-    
+
     def initialize( options={} )
       @options = options
     end
-    
+
     def method_missing( method, *args, &blk )
-      @options[ method ] || template_for( method )
+      @options.has_key?( method ) ? @options[ method ] : template_for( method )
     end
-    
+
     protected
-    
+
     def template_for( method )
       method.bang? ? "{{{#{method.unbang}}}}" : "{{#{method}}}"
     end
